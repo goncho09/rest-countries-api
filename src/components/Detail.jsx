b901f865d309665ea369b6bd23ac3e744f0d9812
goncho09 <gonzalolarrica@gmail.com>
2024-04-25 21:10:47 -0300

    finish detail page

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -48,13 +48,68 @@ export default function Detail({ countryName }) {
             className="w-full"
           />
         </div>
-        <div className="flex ml-32 pt-8 flex-col">
+        <div className="flex ml-24 pt-8 flex-col">
           <h1 className="text-2xl font-bold">{country.name?.common}</h1>
-          <div className="flex mt-8">
-            <p>
-              <strong>Native name : </strong>{' '}
-              {country.name?.nativeName[0]?.common}
-            </p>
+          <div className="flex gap-x-7">
+            <div className="flex mt-8 flex-col">
+              <p>
+                <strong>Native name : </strong>
+                {country.name?.official}
+              </p>
+              <p>
+                <strong>Population : </strong>
+                {country.population}
+              </p>
+              <p>
+                <strong>Region : </strong>
+                {country.region}
+              </p>
+              <p>
+                <strong>Sub Region : </strong>
+                {country.subregion}
+              </p>
+              <p>
+                <strong>Capital : </strong>
+                {country.capital}
+              </p>
+            </div>
+            <div className="flex mt-8 flex-col">
+              <p>
+                <strong>Top Level Domain : </strong>
+                {country.tld}
+              </p>
+              <p>
+                <strong>Currencies : </strong>
+                {Object.values(country.currencies || {}).map((currencie) => {
+                  return (
+                    <p className="inline">
+                      {currencie.name} {'  '}
+                    </p>
+                  );
+                })}
+              </p>
+              <p>
+                <strong>Languages : </strong>
+                {Object.values(country.languages || {}).map((language) => {
+                  return (
+                    <p className="inline">
+                      {language} {'  '}
+                    </p>
+                  );
+                })}
+              </p>
+            </div>
+          </div>
+
+          <div className=" flex items-center space-x-3 mt-32">
+            <strong>Border countries : </strong>
+            {Object.values(country.borders || {}).map((border) => {
+              return (
+                <p className="border border-gray-400 p-1  rounded-md">
+                  {border} {'  '}
+                </p>
+              );
+            })}
           </div>
         </div>
       </div>
